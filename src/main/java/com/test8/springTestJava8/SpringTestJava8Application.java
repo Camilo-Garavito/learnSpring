1b0b9293e57f7c455dbdd0601cde4711c90cf46c
Camilo Andres Garavito <cagaravito@vectoritcgroup.com>
2018-02-02 18:03:21 -0500

    Upload SpringInfo
    Here is my first try with the GitHub, trying to give a kickoff to my Spring path

---
+++ src/main/java/com/test8/springTestJava8/SpringTestJava8Application.java
@@ -0,0 +1,12 @@
+package com.test8.springTestJava8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTestJava8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTestJava8Application.class, args);
+	}
+}
